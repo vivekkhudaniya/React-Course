ba0f3a6a9fe94a3feb584f4f1446a13221bcd710
vivekkhudaniya <vivekkhudaniya@gmail.com>
2023-06-10 15:49:57 +0530

    setState

--- src/App.js
+++ src/App.js
@@ -4,24 +4,30 @@ import React, { useState } from 'react';
 import Component1 from './components/Component1';
 
 function App() {
-
   const [state, setState] = useState({ counter: 0 });
 
-  const counter = 0;
-
   const increment = () => {
-    setState({counter : 5});
+    setState(prevState => ({ counter: prevState.counter + 1 }));
   }
-
 
   return (
     <div>
       <h1>Counter: {state.counter}</h1>
       <button onClick={increment}>Increment</button>
     
-      <Component1 name = "Vivek" age = {25}/>
+      <Component1 name="Vivek" age={25} />
+
+      <div>
+        React
+        {
+          false
+          ? true ? <div>Hey1</div> : <div>Hey2 </div>
+          : true ? <div>Hey3</div> : <div>Hey4</div>
+        }
+      </div>
     </div>
   );
+
 }
 
 export default App;
